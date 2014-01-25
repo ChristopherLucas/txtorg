76bef94251ff45c490e91f1717617819a11cdd95
ChristopherLucas <clucas@fas.harvard.edu>
2014-01-25 11:35:35 -0500

    Revised automatic spellchecker

--- textorganizer/preprocessing.py
+++ textorganizer/preprocessing.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-# This script hold the various preprocessing functions. Most of them find their way into the workflow in indexfiles.py.
-# Email Chris for questions
+# This script hold the various preprocessing functions. They fold into indexfiles (for importing whole docs) or 
+# addmetadata (for importing from a CSV)
+
 import chardet
+import re, collections
 
 'encodings': self.encodings.get(), 
 
@@ -18,6 +20,9 @@ def preprocess(contents, args_dir):
     
     return(processed_contents)
 
+###################
+###################
+
 def convert_encodings(contents, args_dir):
     if args_dir['encodings'] == 'DETECT':
         encoding = chardet.detect(contents)['encoding']
@@ -28,8 +33,51 @@ def convert_encodings(contents, args_dir):
     
     return(contents)
 
+###################
+###################
+
 def dictionary_replacement(contents, args_dir):
-    
+    if args_dir['autocorrect'] == '1':
+        
+
+    return(contents)
+
+###################
+###################
 
 def automated_english_spellcheck(contents, args_dir):
-    
+    if args_dir['encodings'] == 'DETECT':
+        tokens = re.findall(r"[\w']+|[.,!?;]", contents)
+        for t in tokens:
+            tokens[t] = correct(tokens[t])
+        return(' '.join(tokens))
+
+# Copied this from Peter Norvig
+def words(text): return re.findall('[a-z]+', text.lower()) 
+
+def train(features):
+    model = collections.defaultdict(lambda: 1)
+    for f in features:
+        model[f] += 1
+    return model
+
+NWORDS = train(words(file('training_text.txt').read()))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def edits1(word):
+   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+   deletes    = [a + b[1:] for a, b in splits if b]
+   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(deletes + transposes + replaces + inserts)
+
+def known_edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+
+def known(words): return set(w for w in words if w in NWORDS)
+
+def correct(word):
+    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+    return max(candidates, key=NWORDS.get)
