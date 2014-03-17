453c9821c6293be1a23ad824276b10d1056c00ff
ChristopherLucas <clucas@fas.harvard.edu>
2014-03-17 10:06:30 -0400

    Fixed luceneinstall error

---
+++ installation/lucene_install.py
@@ -0,0 +1,3 @@
+from setuptools.command import easy_install
+easy_install.main( ["-U","https://pylucene-extra.apache-extras.org.codespot.com/files/JCC-2.15-py2.7-win32.egg"] )
+easy_install.main( ["-U","https://pylucene-extra.apache-extras.org.codespot.com/files/lucene-3.6.2-py2.7-win32.egg"] )
