a3cc45141b42fdd02e30ab525f04e568d32009aa
ChristopherLucas <clucas@fas.harvard.edu>
2014-05-21 19:30:13 -0400

    Trying to fix unicode error

--- textorganizer/searchfiles.py
+++ textorganizer/searchfiles.py
@@ -21,7 +21,6 @@ class DictUnicodeWriter(object):
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, D):
-        print D
         self.writer.writerow({k:v.encode('utf8') for k,v in D.items()})
 
         # Fetch UTF-8 output from the queue ...
